dfa147077004c25d4b98e822fff8113e3c688b28
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2026-08-14 13:14:07 +0200

    Update 99_qiime2_V3V4_database.sh

--- 00_scripts/99_qiime2_V3V4_database.sh
+++ 00_scripts/99_qiime2_V3V4_database.sh
@@ -18,43 +18,66 @@ conda activate rachis-qiime2-2026.7
 
 cd /home/plstenge/Grand_Saint_Antoine/taxonomy
 
-# Code from: https://forum.qiime2.org/t/processing-filtering-and-evaluating-the-silva-database-and-other-reference-sequence-data-with-rescript/15494
-qiime rescript get-silva-data \
-    --p-version '138.2' \
-    --p-target 'SSURef_NR99' \
-    --o-silva-sequences silva-138.2-ssu-nr99-rna-seqs.qza \
-    --o-silva-taxonomy silva-138.2-ssu-nr99-tax.qza
+## Code from: https://forum.qiime2.org/t/processing-filtering-and-evaluating-the-silva-database-and-other-reference-sequence-data-with-rescript/15494
+#qiime rescript get-silva-data \
+#    --p-version '138.2' \
+#    --p-target 'SSURef_NR99' \
+#    --o-silva-sequences silva-138.2-ssu-nr99-rna-seqs.qza \
+#    --o-silva-taxonomy silva-138.2-ssu-nr99-tax.qza
+#
+## If you'd like to be able to jump to steps that only take FeatureData[Sequence] as input you can convert your data to FeatureData[Sequence] like so:
+#qiime rescript reverse-transcribe \
+#    --i-rna-sequences silva-138.2-ssu-nr99-rna-seqs.qza \
+#    --o-dna-sequences silva-138.2-ssu-nr99-seqs.qza
+#
+## “Culling” low-quality sequences with cull-seqs
+## Here we’ll remove sequences that contain 5 or more ambiguous bases (IUPAC compliant ambiguity bases) and any homopolymers that are 8 or more bases in length. These are the default parameters.
+#qiime rescript cull-seqs \
+#    --i-sequences silva-138.2-ssu-nr99-seqs.qza \
+#    --o-clean-sequences silva-138.2-ssu-nr99-seqs-cleaned.qza
+#
+## Filtering sequences by length and taxonomy
+#qiime rescript filter-seqs-length-by-taxon \
+#    --i-sequences silva-138.2-ssu-nr99-seqs-cleaned.qza \
+#    --i-taxonomy silva-138.2-ssu-nr99-tax.qza \
+#    --p-labels Archaea Bacteria Eukaryota \
+#    --p-min-lens 900 1200 1400 \
+#    --o-filtered-seqs silva-138.2-ssu-nr99-seqs-filt.qza \
+#    --o-discarded-seqs silva-138.2-ssu-nr99-seqs-discard.qza 
+#
+##Dereplicating in uniq mode
+#qiime rescript dereplicate \
+#    --i-sequences silva-138.2-ssu-nr99-seqs-filt.qza  \
+#    --i-taxa silva-138.2-ssu-nr99-tax.qza \
+#    --p-mode 'uniq' \
+#    --o-dereplicated-sequences silva-138.2-ssu-nr99-seqs-derep-uniq.qza \
+#    --o-dereplicated-taxa silva-138.2-ssu-nr99-tax-derep-uniq.qza
+#
+#
+#qiime feature-classifier fit-classifier-naive-bayes \
+#  --i-reference-reads  silva-138.2-ssu-nr99-seqs-derep-uniq.qza \
+#  --i-reference-taxonomy silva-138.2-ssu-nr99-tax-derep-uniq.qza \
+#  --o-classifier silva-138.2-ssu-nr99-classifier.qza    
 
-# If you'd like to be able to jump to steps that only take FeatureData[Sequence] as input you can convert your data to FeatureData[Sequence] like so:
-qiime rescript reverse-transcribe \
-    --i-rna-sequences silva-138.2-ssu-nr99-rna-seqs.qza \
-    --o-dna-sequences silva-138.2-ssu-nr99-seqs.qza
+qiime feature-classifier extract-reads \
+    --i-sequences silva-138.2-ssu-nr99-seqs-derep-uniq.qza \
+    --p-f-primer GTGYCAGCMGCCGCGGTAA \
+    --p-r-primer CCGYCAATTYMTTTRAGTTT \
+    --p-n-jobs 2 \
+    --p-read-orientation 'forward' \
+    --o-reads silva-138.2-ssu-nr99-seqs-515f-926r.qza
 
-# “Culling” low-quality sequences with cull-seqs
-# Here we’ll remove sequences that contain 5 or more ambiguous bases (IUPAC compliant ambiguity bases) and any homopolymers that are 8 or more bases in length. These are the default parameters.
-qiime rescript cull-seqs \
-    --i-sequences silva-138.2-ssu-nr99-seqs.qza \
-    --o-clean-sequences silva-138.2-ssu-nr99-seqs-cleaned.qza
-
-# Filtering sequences by length and taxonomy
-qiime rescript filter-seqs-length-by-taxon \
-    --i-sequences silva-138.2-ssu-nr99-seqs-cleaned.qza \
-    --i-taxonomy silva-138.2-ssu-nr99-tax.qza \
-    --p-labels Archaea Bacteria Eukaryota \
-    --p-min-lens 900 1200 1400 \
-    --o-filtered-seqs silva-138.2-ssu-nr99-seqs-filt.qza \
-    --o-discarded-seqs silva-138.2-ssu-nr99-seqs-discard.qza 
-
-#Dereplicating in uniq mode
 qiime rescript dereplicate \
-    --i-sequences silva-138.2-ssu-nr99-seqs-filt.qza  \
-    --i-taxa silva-138.2-ssu-nr99-tax.qza \
+    --i-sequences silva-138.2-ssu-nr99-seqs-515f-926r.qza \
+    --i-taxa silva-138.2-ssu-nr99-tax-derep-uniq.qza \
     --p-mode 'uniq' \
-    --o-dereplicated-sequences silva-138.2-ssu-nr99-seqs-derep-uniq.qza \
-    --o-dereplicated-taxa silva-138.2-ssu-nr99-tax-derep-uniq.qza
-
-
+    --o-dereplicated-sequences silva-138.2-ssu-nr99-seqs-515f-926r-uniq.qza \
+    --o-dereplicated-taxa  silva-138.2-ssu-nr99-tax-515f-926r-derep-uniq.qza
+  
+# Aim: Create a scikit-learn naive_bayes classifier for reads
 qiime feature-classifier fit-classifier-naive-bayes \
-  --i-reference-reads  silva-138.2-ssu-nr99-seqs-derep-uniq.qza \
-  --i-reference-taxonomy silva-138.2-ssu-nr99-tax-derep-uniq.qza \
-  --o-classifier silva-138.2-ssu-nr99-classifier.qza    
+    --i-reference-reads silva-138.2-ssu-nr99-seqs-515f-926r-uniq.qza \
+    --i-reference-taxonomy silva-138.2-ssu-nr99-tax-515f-926r-derep-uniq.qza \
+    --o-classifier silva-138.2-ssu-nr99-515f-926r-classifier.qza
+
+
